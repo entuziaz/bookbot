34929c79044bf02c819ddefa09b19549be58014b
jkaylight <jkaylight@gmail.com>
2025-07-10 03:02:48 +0100

    bookbot done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_list
 
 def get_book_text(f): 
     file_contents = f.read()
@@ -9,11 +9,20 @@ def main():
         book_content = get_book_text(f) 
         word_count = count_words(book_content)
         char_count = count_characters(book_content)
+        sorted_list = sort_list(char_count)
 
-        print(f"{word_count} words found in the document") 
-        print(f"{char_count} characters found in the document") 
+        # print(f"{word_count} words found in the document") 
+        # print(f"{char_count} characters found in the document") 
 
-
-
+        print(f"""
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {word_count} total words
+--------- Character Count -------""")
+        for char in sorted_list:
+            if char['char'].isalpha():
+                print(f"{char['char']}: {char['num']}")
+        print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -5,7 +5,6 @@ def count_words(text):
 
 def count_characters(text):
     lower_case = text.lower()
-    
     # following could be replaced with collections.Counter
     # such that: return Counter(lower_case)
     counted_chars = {}
@@ -14,5 +13,17 @@ def count_characters(text):
             counted_chars[letter] += 1
         else:
             counted_chars[letter] = 1
-
     return counted_chars
+
+def sort_on(items):
+    return items["num"]
+
+def sort_list(counted_chars):
+    new_list = []
+    for item in counted_chars:
+        char = item 
+        num = counted_chars[item]
+        new_list.append({"char": char, "num": num})
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
+    
